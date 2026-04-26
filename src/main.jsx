9a95934154987062cdc33b45c357b07743a1b1df
Paul Quemado <paulquemado@gmail.com>
2026-04-25 23:28:24 -0500

    remove amplify outputs dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Amplify } from 'aws-amplify'
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
 import './index.css'
-import outputs from '../amplify_outputs.json'
-
-Amplify.configure(outputs)
 
 function App() {
   return (
@@ -14,15 +8,7 @@ function App() {
       <h1>Profiles App</h1>
       <h2>Paul Quemado</h2>
       <p>Task 5 submission screenshot</p>
-
-      <Authenticator>
-        {({ user, signOut }) => (
-          <main>
-            <p>You are signed in as: {user?.signInDetails?.loginId}</p>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
-      </Authenticator>
+      <button>Hello from Task 5</button>
     </div>
   )
 }
