cb248dcc37b504a9b3aa93ebb133887c321138fa
Paul Quemado <paulquemado@gmail.com>
2026-04-25 23:10:25 -0500

    add data model

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -1,5 +1,21 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { postConfirmation } from '../auth/post-confirmation/resource';
 
-export const postConfirmation = defineFunction({
-  name: 'post-confirmation',
+const schema = a.schema({
+  UserProfile: a
+    .model({
+      email: a.email().required(),
+      fullName: a.string(),
+      owner: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
+  },
 });
