c54c0481c29bcd28a9a9390c7b723204a2d0416a
Paul Quemado <paulquemado@gmail.com>
2026-04-25 23:14:52 -0500

    link post confirmation trigger

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -1,21 +1,11 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-import { postConfirmation } from '../auth/post-confirmation/resource';
+import { defineAuth } from '@aws-amplify/backend';
+import { postConfirmation } from './post-confirmation/resource';
 
-const schema = a.schema({
-  UserProfile: a
-    .model({
-      email: a.email().required(),
-      fullName: a.string(),
-      owner: a.string(),
-    })
-    .authorization((allow) => [allow.owner()]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  triggers: {
+    postConfirmation,
   },
 });
